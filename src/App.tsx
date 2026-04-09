f731c1002734287c810da8860ff27f932d3f9b96
lxvxxu <lxvxxu@gmail.com>
2026-04-09 17:48:16 +0900

    Merge develop into chore/3 (resolve conflicts)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import LoginPage from "./pages/Auth/LoginPage";
-// import SignupPage from "./pages/Auth/SignupPage";
+import SignupPage from "./pages/Auth/SignupPage";
 
 function App() {
   return (
@@ -10,8 +10,7 @@ function App() {
         {/* 인증 */}
         {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="/login" element={<div>로그인</div>} />
-        {/* <Route path="/signup" element={<SignupPage />} /> */}
-        <Route path="/signup" element={<div>회원가입</div>} />
+        {<Route path="/signup" element={<SignupPage />} />}
         <Route path="/guest-verify" element={<div>비회원 예매 인증</div>} />
         {/* 공연 */}
         <Route path="/" element={<div>공연 목록(메인 화면)</div>} />
